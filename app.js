c19004bce0fd2adde3f74d0a8de8d785ed988c59
neo0910 <denikkalenik@gmail.com>
2019-09-06 20:14:35 +0300

    added NODE_ENV variable

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
